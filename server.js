7b3a66ba740c0a60bf2aef807ae2540681bd624d
BayelChieftain <marvel.vovo@gmail.com>
2024-02-17 23:02:29 +0600

    build server and get users

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const admin = require('firebase-admin');
+const serviceAccount = require('./user-management-f931a-594254e73733.json');
+
+const app = express();
+const port = 3001;
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: process.env.FIREBASE_ID_PROJECT,
+});
+
+
+app.get('/api/users', async (req, res) => {
+  try {
+    const listUsersResult = await admin.auth().listUsers();
+    const users = listUsersResult.users.map((userRecord) => ({
+      uid: userRecord.uid,
+      email: userRecord.email,
+      displayName: userRecord.displayName,
+      lastSignInTime: userRecord.metadata.lastSignInTime,
+    }));
+
+    console.log('List of users:', users);
+    res.json(users);
+  } catch (error) {
+    console.error('Error fetching users:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
+});
